0b712659ea3544c9c5f16d19438f4b296f2232e7
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-01-16 12:40:20 +0700

    feat(fetching): Send fetch request

--- 03-fetching/src/main/java/io/spring/fetching/FetchingApplication.java
+++ 03-fetching/src/main/java/io/spring/fetching/FetchingApplication.java
@@ -1,13 +1,39 @@
 package io.spring.fetching;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FetchingApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(FetchingApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(FetchingApplication.class, args);
 	}
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject("http://localhost:14045/api/random", Quote.class);
+
+            if (quote == null) {
+                throw new Exception();
+            }
+
+            log.info(quote.toString());
+        };
+    }
 
 }
