aeaa05b1d6c215f2b795a19a68cf5b678c2d935f
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-01-16 11:03:09 +0700

    chore(fetching): Initial project scaffold

---
+++ 03-fetching/src/main/java/io/spring/fetching/FetchingApplication.java
@@ -0,0 +1,13 @@
+package io.spring.fetching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchingApplication.class, args);
+	}
+
+}
