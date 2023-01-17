88965965e4f95014de7eb8984f6b6713fe1f3228
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-01-17 07:44:37 +0700

    chore(dbsql): Initial project scaffold

---
+++ 05-dbsql/src/main/java/io/spring/dbsql/DbsqlApplication.java
@@ -0,0 +1,13 @@
+package io.spring.dbsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsqlApplication.class, args);
+	}
+
+}
---
+++ 05-dbsql/src/test/java/io/spring/dbsql/DbsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.dbsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
