6b2c6f5dd9dc653a29928f515835d752fae199c9
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-01-18 11:40:59 +0700

    feat(tutorials): Add `getAllTutorials` request handler

--- 06-tutorials/src/main/java/com/bezkoder/tutorials/tutorial/TutorialController.java
+++ 06-tutorials/src/main/java/com/bezkoder/tutorials/tutorial/TutorialController.java
@@ -3,10 +3,14 @@ package com.bezkoder.tutorials.tutorial;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -22,6 +26,17 @@ public class TutorialController {
                     .save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
 
             return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials() {
+        try {
+            List<Tutorial> tutorials = new ArrayList<>(tutorialRepository.findAll());
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
