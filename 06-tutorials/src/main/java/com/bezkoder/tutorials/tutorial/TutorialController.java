5483427ec0116662bca184eeb33dd3663de9b217
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-01-18 12:52:45 +0700

    feat(tutorials): Add `deleteAllTutorials` request handler

--- 06-tutorials/src/main/java/com/bezkoder/tutorials/tutorial/TutorialController.java
+++ 06-tutorials/src/main/java/com/bezkoder/tutorials/tutorial/TutorialController.java
@@ -63,4 +63,14 @@ public class TutorialController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @DeleteMapping("/tutorials")
+    public ResponseEntity<HttpStatus> deleteAllTutorials() {
+        try {
+            tutorialRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
