3b93cc17cb227b616d134282c31484728f11e502
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-01-23 08:33:00 +0700

    feat(tutorials): Define `Comment` model and its many to one relationship to `Tutorial`

--- 06-tutorials/src/main/java/com/bezkoder/tutorials/model/Tutorial.java
+++ 06-tutorials/src/main/java/com/bezkoder/tutorials/model/Tutorial.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class Tutorial {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tutorial_generator")
     private long id;
 
     @Column(name = "title")
